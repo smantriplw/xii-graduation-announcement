4fa1799c6231e49551e750397849ea3f25574f15
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-04-19 08:14:16 +0000

    feat: init
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+## XII Graduation Announcement
--- config/jwt.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace SMANTI\Config;
-
-class JWTConfig
-{
-    const secretKey = "smantipeduli_2023"; // change this
-    const issuer    = "graduation.sman3palu.sch.id";
-}
