d509a669bac7252ca4074ee9b7b36d3c34a6b287
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-04-19 09:08:38 +0000

    feat: add jetstream, and graduated student model
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ app/Models/GraduatedStudent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GraduatedStudent extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'nisn',
+        'birth',
+        'graduate_year',
+        'photo_path',
+    ];
+
+    protected $casts = [
+        'birth' => 'date:dd-MM-YYYY',
+    ];
+}
---
+++ database/migrations/2023_04_19_090742_graduated_student.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('graduated_students', function(Blueprint $table) {
+            $table->id();
+            $table->integer('nisn');
+            $table->string('name');
+            $table->date('birth');
+            $table->string('photo_path');
+            $table->integer('graduate_year');
+            
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('graduated_students');
+    }
+};
