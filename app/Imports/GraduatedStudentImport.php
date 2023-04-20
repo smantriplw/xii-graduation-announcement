14e352b558e56b1b5bbdc39e232ad0945c968ad5
hansputera <hanifdwyputrasembiring@gmail.com>
2023-04-20 13:24:28 +0800

    feat(GraduatedStudentImport): extend WithHeadingRow
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- app/Imports/GraduatedStudentImport.php
+++ app/Imports/GraduatedStudentImport.php
@@ -5,9 +5,10 @@ namespace App\Imports;
 use App\Models\GraduatedStudent;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
 
-class GraduatedStudentImport implements ToModel, WithValidation
+class GraduatedStudentImport implements ToModel, WithValidation, WithHeadingRow
 {
     use Importable;
     /**
