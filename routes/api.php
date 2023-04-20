f0e0290d17e53e2234d00be3a35b90ae70ceb785
hansputera <hanifdwyputrasembiring@gmail.com>
2023-04-20 14:32:40 +0800

    chore: use Api name instead rather than Admin
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\GraduatedStudentController;
+use App\Http\Controllers\Api\GraduatedStudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
