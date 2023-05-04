4faf3ec81960a8362c65a3c1ff1366eb88abe338
hanifdwyputras <hanifdwyputrasembiring@gmail.com>
2023-05-04 19:24:53 +0000

    refinal
    Signed-off-by: hanifdwyputras <hanifdwyputrasembiring@gmail.com>

---
+++ public/build/assets/AuthenticationCardLogo-781e2ee8.js
@@ -0,0 +1,1 @@
+import{o as e,c as a,w as o,u as t,j as s,a as c}from"./app-158127d5.js";const n=c("img",{src:"/images/logo.png",class:"object-contain h-32 lg:h-56"},null,-1),u={__name:"AuthenticationCardLogo",setup(r){return(_,i)=>(e(),a(t(s),{href:"/"},{default:o(()=>[n]),_:1}))}};export{u as _};
